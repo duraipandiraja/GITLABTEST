a19075c371cb97af4432402145f645d08bfd8f4e
DURAI <DURAI@DESKTOP-KJ6UEMM>
2019-02-19 22:31:37 +0530

    INITIAL VERSION

---
+++ src/main/java/DURAI/DURAI/App.java
@@ -0,0 +1,13 @@
+package DURAI.DURAI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
